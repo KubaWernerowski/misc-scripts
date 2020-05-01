bca7be3fb2690a46014520ce0c67a560abb9f226
Kuba Wernerowski <kubawernerowski@gmail.com>
2020-05-01 19:25:07 -0400

    Finished script. Works great

--- heic-convert/heic-to-png.bash
+++ heic-convert/heic-to-png.bash
@@ -1,3 +1,19 @@
-cd ~/Downloads 
-magick convert *.HEIC *.png
-rm *.HEIC
+#!/bin/bash
+
+# variables
+DATE=$(date +%F)
+IN_DIR='/Users/kuba/Downloads/'
+BASE_DIR='/Users/kuba/Documents/photos/HEIC-originals/'
+OUT_DIR=$BASE_DIR$DATE
+
+mkdir -p $OUT_DIR
+
+cd $IN_DIR
+
+list=$(ls *.HEIC)
+for img in $list
+do 
+    mogrify -format png -resize 75% $img
+    mv $img $OUT_DIR
+done
+
